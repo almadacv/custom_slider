dca4cbe743c895a3b6b8218c3a48794354f6fa6d
almadacv <emealmada@gmail.com>
2021-09-29 02:53:27 -0100

    class_thumb_fixed

--- custom_slider.php
+++ custom_slider.php
@@ -99,7 +99,6 @@ function custom_slider($atts)
 
 	global $post;
 
-
 	// WP_Query arguments
 	$args = array(
 		//'post_type'              => array('radios'),
@@ -112,7 +111,6 @@ function custom_slider($atts)
 		'order'                  => 'ASC',
 		'orderby'                => 'title',
 		'post__not_in' => array($post->ID),
-		'dots' => 'true',
 	);
 
 	// The Query
@@ -127,13 +125,10 @@ function custom_slider($atts)
 				while ($query->have_posts()) {
 					$query->the_post();
 					// do something
-					$image = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'large');
 			?>
 					<div class="slider_elemento">
 						<a href="<?php the_permalink() ?> ">
-							<div class="slider_img"><?php the_post_thumbnail('post-thumbnail', array('class' => '.elementor-post__thumbnail')) ?> </div>
-							<!--<div class="slider_img"><? //php the_post_thumbnail('post-thumbnail', array('class' => '.elementor-post__thumbnail, .elementor-fit-height, .lazyloaded')) 
-														?> </div>-->
+							<div class="slider_img"><?php the_post_thumbnail('post-thumbnail', array('class' => 'elementor-post__thumbnail lazyloaded elementor-fit-height')) ?> </div>
 							<div class="slider_texto"> <?php the_title('<p class="elementor-post__title"">', '</p>') ?></div>
 						</a>
 					</div>
@@ -141,10 +136,8 @@ function custom_slider($atts)
 		</div>
 	</div>
 
-
 	<script>
 		var $jq = jQuery.noConflict();
-
 		$jq('.slider_gera').each(function(index) {
 			$jq(this).attr('data-slider', index);
 			$jq(this).not('.slick-initialized').slick({
@@ -156,8 +149,6 @@ function custom_slider($atts)
 				appendArrows: this.closest('.container_slider'),
 				prevArrow: '<div class="slider-prev fa fa-chevron-left fa-2x"></div>',
 				nextArrow: '<div class="slider-next fa fa-chevron-right fa-2x"></div>',
-				//			prevArrow: '<div class="slider-prev fa fa-angle-left fa-2x"></div>',
-				//			nextArrow: '<div class="slider-next fa fa-angle-right fa-2x"></div>',
 				responsive: [{
 						breakpoint: 1024,
 						settings: {
@@ -235,10 +226,8 @@ function custom_slider($atts)
 			} else {
 				echo 'no posts found';
 			}
-
 			// Restore original Post Data
 			wp_reset_postdata();
 		}
-
 		add_shortcode('custom-slider-teste', 'custom_slider');
 ?>

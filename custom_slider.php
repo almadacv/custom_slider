7139c3b00faaecc1167f641bf12fe695d368128b
almadacv <emealmada@gmail.com>
2021-09-29 02:39:15 -0100

    add mas k 1 slider na mesmo pagina

--- custom_slider.php
+++ custom_slider.php
@@ -99,15 +99,12 @@ function custom_slider($atts)
 
 	global $post;
 
-	$Content = "";
 
 	// WP_Query arguments
 	$args = array(
 		//'post_type'              => array('radios'),
 		'post_type'              => $atts['tipo'],
 		'post_status'            => array('Publish'),
-		//'category_name'          => 'Patrocinado',
-		//		'category_name'          => 'Não Patrocinado',
 		'category_name'          => $atts['cate'],
 		'nopaging'               => false,
 		//'posts_per_page'         => '4',
@@ -115,6 +112,7 @@ function custom_slider($atts)
 		'order'                  => 'ASC',
 		'orderby'                => 'title',
 		'post__not_in' => array($post->ID),
+		'dots' => 'true',
 	);
 
 	// The Query
@@ -123,7 +121,6 @@ function custom_slider($atts)
 	// The Loop
 	ob_start();
 ?>
-
 	<div class="container_slider">
 		<div class="slider_gera">
 			<?php if ($query->have_posts()) {
@@ -134,7 +131,9 @@ function custom_slider($atts)
 			?>
 					<div class="slider_elemento">
 						<a href="<?php the_permalink() ?> ">
-							<div class="slider_img"><?php the_post_thumbnail('post-thumbnail', array('class' => '.card-img-top, .lazyloaded')) ?> </div>
+							<div class="slider_img"><?php the_post_thumbnail('post-thumbnail', array('class' => '.elementor-post__thumbnail')) ?> </div>
+							<!--<div class="slider_img"><? //php the_post_thumbnail('post-thumbnail', array('class' => '.elementor-post__thumbnail, .elementor-fit-height, .lazyloaded')) 
+														?> </div>-->
 							<div class="slider_texto"> <?php the_title('<p class="elementor-post__title"">', '</p>') ?></div>
 						</a>
 					</div>
@@ -142,55 +141,94 @@ function custom_slider($atts)
 		</div>
 	</div>
 
+
 	<script>
 		var $jq = jQuery.noConflict();
 
-		$jq('.slider_gera').slick({
-			infinite: true,
-			slidesToShow: 4,
-			slidesToScroll: 3,
-			/*adaptiveHeight: true,
-			variableWidth:true,
-			autoplay: true,
-			autoplayspeed: 2000, */
-			arrows: true,
-			dots: true,
-			// Add FontAwesome Class
-			appendArrows: '.container_slider',
-			prevArrow: '<div class="slider-prev fa fa-chevron-left"></div>',
-			nextArrow: '<div class="slider-next fa fa-chevron-right"></div>',
-			//			prevArrow: '<div class="slider-prev fa fa-angle-left"></div>',
-			//			nextArrow: '<div class="slider-next fa fa-angle-right"></div>',
-			responsive: [{
-					breakpoint: 1024,
-					settings: {
-						slidesToShow: 3,
-						slidesToScroll: 3,
-						infinite: true,
-						dots: true
-					}
-				},
-				{
-					breakpoint: 600,
-					settings: {
-						slidesToShow: 2,
-						slidesToScroll: 2
-					}
-				},
-				{
-					breakpoint: 480,
-					settings: {
-						slidesToShow: 1,
-						slidesToScroll: 1
-					}
-				}
-				// You can unslick at a given breakpoint now by adding:
-				// settings: "unslick"
-				// instead of a settings object
-			]
-
+		$jq('.slider_gera').each(function(index) {
+			$jq(this).attr('data-slider', index);
+			$jq(this).not('.slick-initialized').slick({
+				infinite: true,
+				slidesToShow: 4,
+				slidesToScroll: 3,
+				arrows: true,
+				dots: true,
+				appendArrows: this.closest('.container_slider'),
+				prevArrow: '<div class="slider-prev fa fa-chevron-left fa-2x"></div>',
+				nextArrow: '<div class="slider-next fa fa-chevron-right fa-2x"></div>',
+				//			prevArrow: '<div class="slider-prev fa fa-angle-left fa-2x"></div>',
+				//			nextArrow: '<div class="slider-next fa fa-angle-right fa-2x"></div>',
+				responsive: [{
+						breakpoint: 1024,
+						settings: {
+							slidesToShow: 3,
+							slidesToScroll: 3,
+							infinite: true,
+							dots: true
+						}
+					},
+					{
+						breakpoint: 600,
+						settings: {
+							slidesToShow: 2,
+							slidesToScroll: 2
+						}
+					},
+				]
+			});
 		});
 	</script>
+
+	<style>
+		.slider_texto p {
+			overflow: hidden;
+			text-overflow: ellipsis;
+			line-height: 1.3em;
+			height: 2.6em;
+			font-family: "Poppins", poppins;
+			font-size: 15px;
+			font-weight: 600;
+			color: #28303d;
+			display: -webkit-box;
+			-webkit-box-orient: vertical;
+			-webkit-line-clamp: 2;
+		}
+
+		.slider_elemento {
+			margin: 5px;
+		}
+
+		.slider_img img {
+			/* background-color: green; */
+			/*border: 1px #888 solid;*/
+			box-shadow: 0px 0px 2px 0px rgb(0 0 0 / 50%);
+			border-radius: 8px;
+			margin-bottom: 20px;
+		}
+
+		.slider-prev,
+		.slider-next {
+			/* background-color: red; */
+		}
+
+		.slick-dots {
+			/* background-color: cyan; */
+		}
+
+		.container_slider {
+			/* background-color: pink; */
+		}
+
+		.slick-slide {
+			/* background-color: #4899dd; */
+		}
+
+		.slider_elemento {
+			/* border: 1px #888 solid;
+			border-radius: 8px; */
+			margin: 5px;
+		}
+	</style>
 
 <?php
 				ob_end_flush();
@@ -198,11 +236,8 @@ function custom_slider($atts)
 				echo 'no posts found';
 			}
 
-
 			// Restore original Post Data
 			wp_reset_postdata();
-
-			return $Content;
 		}
 
 		add_shortcode('custom-slider-teste', 'custom_slider');
